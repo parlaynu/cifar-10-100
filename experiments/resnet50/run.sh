7edb002ccfa87f3afe468a97f77d622bb810733f
Paul Ryan <paul@reillyryan.net>
2023-01-23 07:42:49 +1100

    add logs from resnet50/cifar-10 experiments

--- experiments/resnet50/run.sh
+++ experiments/resnet50/run.sh
@@ -5,3 +5,8 @@
 
 ../../cifar-10/train.py -w 2 -e 50 -b 64 --lr 0.000005 -s 224 -z 0 resnet50 ~/Projects/datasets/cifar-10
 
+../../cifar-10/train.py -w 2 -e 50 -b 64 --lr 0.00001 -s 224 -z 0 resnet50 ~/Projects/datasets/cifar-10
+../../cifar-10/train.py -w 2 -e 50 -b 64 --lr 0.00001 -s 224 -z 4 resnet50 ~/Projects/datasets/cifar-10
+../../cifar-10/train.py -w 2 -e 50 -b 64 --lr 0.00001 -s 224 -z 5 resnet50 ~/Projects/datasets/cifar-10
+../../cifar-10/train.py -w 2 -e 50 -b 64 --lr 0.00001 -s 224 -z 6 resnet50 ~/Projects/datasets/cifar-10
+
