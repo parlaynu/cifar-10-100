5c1a8de5a471d32439899b38bec83a0930ddc117
Paul Ryan <paul@reillyryan.net>
2023-01-24 10:10:43 +1100

    remove incorrect experiment results

--- experiments/mobilenet_v3_large/run.sh
+++ experiments/mobilenet_v3_large/run.sh
@@ -7,13 +7,12 @@
 ../../train.py -w 2 -e 75 -b 64 --lr 0.0001 mobilenet_v3_large ~/Projects/datasets/cifar-100
 ../../train.py -w 2 -e 75 -b 64 --lr 0.0002 mobilenet_v3_large ~/Projects/datasets/cifar-100
 
-
 # cifar-10 at 224x224
 
 ../../find-lr.py -w 2 -e 4 -b 64 -s 224 -f 0.001 mobilenet_v3_large ~/Projects/datasets/cifar-10
 ../../find-lr.py -w 2 -e 4 -b 64 -s 224 -f 0.0001 mobilenet_v3_large ~/Projects/datasets/cifar-10
 
-../../train.py -w 2 -e 75 -b 64 --lr 0.00001 mobilenet_v3_large ~/Projects/datasets/cifar-10
-../../train.py -w 2 -e 75 -b 64 --lr 0.00005 mobilenet_v3_large ~/Projects/datasets/cifar-10
-../../train.py -w 2 -e 75 -b 64 --lr 0.0001 mobilenet_v3_large ~/Projects/datasets/cifar-10
+../../train.py -w 2 -e 75 -b 64 -s 224 --lr 0.00001 mobilenet_v3_large ~/Projects/datasets/cifar-10
+../../train.py -w 2 -e 75 -b 64 -s 224 --lr 0.00005 mobilenet_v3_large ~/Projects/datasets/cifar-10
+../../train.py -w 2 -e 75 -b 64 -s 224 --lr 0.0001 mobilenet_v3_large ~/Projects/datasets/cifar-10
 
